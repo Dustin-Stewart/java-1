e249ebbbab10deb337295567c07b9f5f175c45ad
Dustin Stewart <drstewa5@uno.edu>
2017-08-30 18:11:00 -0500

    Expand Bouncer to Loop

--- lab_2/Bouncer.java
+++ lab_2/Bouncer.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 public class Bouncer {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int age;
-		System.out.println("how old are you?");
-		age = input.nextInt();
-		if (age < 18) {
-			System.out.println("get TF out of here");
-		}
-		else if (age >= 18 && age < 21) {
-			System.out.println("you can come in, but no drinks");
-		}
-		else {
-			System.out.println("welcome home");
-		}
-	}
+			int numberInLine;
+			System.out.println("How many people are in line?");
+			numberInLine = input.nextInt();
+			while (numberInLine > 0){
+				int age;
+				System.out.println("how old are you?");
+				age = input.nextInt();
+				if (age < 18) {
+					System.out.println("get TF out of here");
+				}
+				else if (age >= 18 && age < 21) {
+					System.out.println("you can come in, but no drinks");
+				}
+				else {
+					System.out.println("welcome home");
+				}
+			
+		System.out.println("Nonetheless, have a great night!");
+		numberInLine--;
+}
+}
 }
